583da35fd099f8d501fc1b114d2308e9e0c0f680
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2021-06-09 01:23:25 -0500

    add application confusion matrix console

--- App.php
+++ App.php
@@ -3,50 +3,17 @@
 /**
  * Aplicación
  *
+ * Execution: ./App.php
+ *
  * @author Gonzalo Chacaltana Buleje <gchacaltanab@outlook.com>
- * @version v1.0
+ * @version v1.0.0
  * @access public
  */
 require_once __DIR__ . '/vendor/autoload.php';
 
-class App {
+use Symfony\Component\Console\Application;
+use Console\ConfusionMatrixCommand;
 
-    /**
-     * @var int True Positive. Verdadero Positivo
-     * @access private
-     */
-    private $_tp;
-
-    /**
-     * @var int True Negative. Verdadero Negativo
-     * @access private
-     */
-    private $_tn;
-
-    /**
-     * @var int False Positive. Falso Positivo
-     * @access private
-     */
-    private $_fp;
-
-    /**
-     * @var int False Negative. Falso Negativo
-     * @access private
-     */
-    private $_fn;
-
-    public function __construct() {
-        $this->_tp = 0;
-        $this->_tn = 0;
-        $this->_fp = 0;
-        $this->_fn = 0;
-    }
-
-    public function run() {
-        print "main";
-    }
-
-}
-
-$app = new App();
+$app = new Application('Confusion Matrix App', 'v1.0.0');
+$app->add(new ConfusionMatrixCommand());
 $app->run();
