f9eff7772e07c13c5314d61b703d0e25b9781bec
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2021-06-09 01:48:41 -0500

    fixed app execution

--- App.php
+++ App.php
@@ -3,7 +3,7 @@
 /**
  * Aplicación
  *
- * Execution: ./App.php
+ * Execution: ./App.php main <TP> <TN> <FP> <FN>
  *
  * @author Gonzalo Chacaltana Buleje <gchacaltanab@outlook.com>
  * @version v1.0.0
@@ -14,6 +14,6 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Symfony\Component\Console\Application;
 use Console\ConfusionMatrixCommand;
 
-$app = new Application('Confusion Matrix App', 'v1.0.0');
+$app = new Application('Matriz de Confusión', 'v1.0.0');
 $app->add(new ConfusionMatrixCommand());
 $app->run();
