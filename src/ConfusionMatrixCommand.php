d99144cd99ff586c6b434b12cff8180713ee9944
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2021-06-13 23:48:50 -0500

    refactoring command main class

--- src/ConfusionMatrixCommand.php
+++ src/ConfusionMatrixCommand.php
@@ -10,13 +10,29 @@
 
 namespace Console;
 
+use Symfony\Component\Console\Command\Command as SymfonyCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Console\ConfusionMatrix;
 
-class ConfusionMatrixCommand extends ConfusionMatrix {
+class ConfusionMatrixCommand extends SymfonyCommand {
+
+    /**
+     * @var InputInterface input
+     */
+    private $_input;
+
+    /**
+     * @var OutputInterface output
+     */
+    private $_output;
+
+    /**
+     * @var object ConfusionMatrix
+     */
+    private $_confusionMatrix = NULL;
 
     public function configure() {
         $this->setName('main')
@@ -29,7 +45,54 @@ class ConfusionMatrixCommand extends ConfusionMatrix {
     }
 
     public function execute(InputInterface $input, OutputInterface $output) {
-        $this->main($input, $output);
+        try {
+            $this->_input = $input;
+            $this->_output = $output;
+            $this->_displayHead();
+            $this->_setAttributes();
+            $this->_displayAttributes();
+            $this->_displayMetrics();
+        } catch (\ErrorException $ex) {
+            $output->writeln($ex->getMessage());
+        }
+    }
+
+    private function _displayHead() {
+        $this->_output->writeln([
+            '',
+            '**** Matriz de Confusión ****',
+            '*****************************'
+        ]);
+    }
+
+    private function _setAttributes() {
+        $tp = intval($this->_input->getArgument('tp'));
+        $tn = intval($this->_input->getArgument('tn'));
+        $fp = intval($this->_input->getArgument('fp'));
+        $fn = intval($this->_input->getArgument('fn'));
+        $this->_confusionMatrix = new ConfusionMatrix($tp, $tn, $fp, $fn);
+    }
+
+    private function _displayAttributes() {
+        $this->_output->writeln([
+            '',
+            'TP: Verdadero Positivo = ' . $this->_confusionMatrix->getTruePositive(),
+            'TN: Verdadero Negativo = ' . $this->_confusionMatrix->getTrueNegative(),
+            'FP: Falso Positivo = ' . $this->_confusionMatrix->getFalsePositive(),
+            'FN: Falso Negativo = ' . $this->_confusionMatrix->getFalseNegative()
+        ]);
+    }
+
+    private function _displayMetrics() {
+        $this->_output->writeln([
+            '',
+            'Sensitivity = ' . $this->_confusionMatrix->getSensitivity(),
+            'Specificity = ' . $this->_confusionMatrix->getSpecificity(),
+            'Precision = ' . $this->_confusionMatrix->getPrecision(),
+            'Accuracy = ' . $this->_confusionMatrix->getAccuracy(),
+            'F1 Score = ' . $this->_confusionMatrix->getF1Score(),
+            'Recall = ' . $this->_confusionMatrix->getRecall()
+        ]);
     }
 
 }
