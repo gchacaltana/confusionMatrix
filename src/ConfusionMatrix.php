3c790df5445782255fcb2cc4c426b953b9e70b05
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2021-06-17 00:34:20 -0500

    add return strict type methods

--- src/ConfusionMatrix.php
+++ src/ConfusionMatrix.php
@@ -1,5 +1,6 @@
 <?php
 
+declare(strict_types = 1);
 /**
  * ConfusionMatrix
  *
@@ -52,7 +53,7 @@ class ConfusionMatrix {
      * @access public
      * @return int VerdaderoPositivo
      */
-    public function getTruePositive() {
+    public function getTruePositive(): int {
         return $this->_tp;
     }
 
@@ -61,7 +62,7 @@ class ConfusionMatrix {
      * @access public
      * @return int Verdadero Negativo
      */
-    public function getTrueNegative() {
+    public function getTrueNegative(): int {
         return $this->_tn;
     }
 
@@ -70,7 +71,7 @@ class ConfusionMatrix {
      * @access public
      * @return int Falso Positivo
      */
-    public function getFalsePositive() {
+    public function getFalsePositive(): int {
         return $this->_fp;
     }
 
@@ -79,56 +80,62 @@ class ConfusionMatrix {
      * @access public
      * @return int Falso Negativo
      */
-    public function getFalseNegative() {
+    public function getFalseNegative(): int {
         return $this->_fn;
     }
 
     /**
      * Devuelve la métrica de sensibilidad
+     * @return float
      */
-    public function getSensitivity() {
+    public function getSensitivity(): float {
         $divisor = $this->_tp + $this->_fn;
-        return ($divisor > 0) ? round($this->_tp / $divisor, 4) : "";
+        return ($divisor > 0) ? round($this->_tp / $divisor, 4) : 0;
     }
 
     /**
      * Devuelve la métrica de especificidad
+     * @return float
      */
-    public function getSpecificity() {
+    public function getSpecificity(): float {
         $divisor = $this->_fp + $this->_tn;
-        return ($divisor > 0) ? round($this->_tn / $divisor, 4) : "";
+        return ($divisor > 0) ? round($this->_tn / $divisor, 4) : 0;
     }
 
     /**
      * Devuelve la métrica de precisión
+     * @return float
      */
-    public function getPrecision() {
+    public function getPrecision(): float {
         $divisor = $this->_tp + $this->_fp;
-        return ($divisor > 0) ? round($this->_tp / ($divisor), 4) : "";
+        return ($divisor > 0) ? round($this->_tp / ($divisor), 4) : 0;
     }
 
     /**
      * Devuelve la métrica de exactitud
+     * @return float
      */
-    public function getAccuracy() {
+    public function getAccuracy(): float {
         $divisor = $this->_tp + $this->_fp + $this->_tn + $this->_fn;
-        return ($divisor > 0) ? round(($this->_tp + $this->_tn) / $divisor, 4) : "";
+        return ($divisor > 0) ? round(($this->_tp + $this->_tn) / $divisor, 4) : 0;
     }
 
     /**
      * Devuelve la métrica de F1 Score
+     * @return float
      */
-    public function getF1Score() {
+    public function getF1Score(): float {
         $divisor = 2 * $this->_tp + $this->_fp + $this->_fn;
-        return ($divisor > 0) ? round((2 * $this->_tp) / $divisor, 4) : "";
+        return ($divisor > 0) ? round((2 * $this->_tp) / $divisor, 4) : 0;
     }
 
     /**
      * Devuelve la métrica de Exhaustividad (Recall)
+     * @return float
      */
-    public function getRecall() {
+    public function getRecall(): float {
         $divisor = $this->_tp + $this->_fn;
-        return ($divisor > 0) ? round($this->_tp / $divisor, 4) : "";
+        return ($divisor > 0) ? round($this->_tp / $divisor, 4) : 0;
     }
 
 }
